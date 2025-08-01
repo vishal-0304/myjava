56b538049d05534a13f2165998874df7faf6a6fa
vishal <vshlzagade@gmail.com>
2025-08-01 14:52:28 +0000

    runcommit

--- myfile.java
+++ myfile.java
@@ -1,5 +1,5 @@
 public class myfile {
     public static void main(String[] args) {
-        System.out.println("Hello, jenkins from visha vishalsl!");
+        System.out.println("good mrng everone!");
     }
 }
